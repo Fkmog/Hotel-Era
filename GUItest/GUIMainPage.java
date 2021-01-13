605eb3345c72bfa16c7669c3c7cba8aac8e916bc
BMO <54570952+Fkmog@users.noreply.github.com>
2021-01-13 13:40:08 +0800

    界面测试
    写了一个窗口框架和界面1，也就是登陆界面

--- GUItest/GUIMainPage.java
+++ GUItest/GUIMainPage.java
@@ -1,12 +1,32 @@
-package Hotel_Era.GUItest;
-/**
- * @author: fkmog
- 
- * @version:2021年1月4日下午8:36:07
- 
- * @description:
- */
+package GUItest;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class GUIMainPage extends JFrame implements ActionListener{
+    public static JFrame HotelEra;
+    public JTextArea textArea;
 
-public class GUIMainPage {
+    public HotelEra()
+    {
+        //窗口框架
+        HotelEra = new JFrame();
+        HotelEra.setTitle("Hotel Era: Hotel Booking System");
+        HotelEra.setBounds(600,300,500,400);
+        HotelEra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //界面1
+        JPanel panel = new JPanel();
+        HotelEra.getContentPane().add(panel,BorderLayout.SOUTH);
+        JButton button1 = new JButton("登陆");
+        JButton button2 = new JButton("注册");
+
+        panel.add(button1);
+        panel.add(button2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
